8f64092ff390dd6b554e64ab4a8e0b109ece22a1
Wietse Franssen <wietse.franssen@wur.nl>
2026-03-13 15:18:18 +0100

    update

--- include/config.h
+++ include/config.h
@@ -22,6 +22,8 @@
 #define TZ_INFO "CET-1CEST,M3.5.0/2,M10.5.0/3"
 #endif
 
-// Hardware instellingen
+// Hardware instellingen – Gledopto ESP32 PWM WLED LED
 static constexpr int LED_CHANNEL_COUNT = 5;
-static constexpr int LED_PINS[LED_CHANNEL_COUNT] = {25, 26, 27, 32, 33};
+// static constexpr int LED_PINS[LED_CHANNEL_COUNT] = {25, 26, 27, 32, 33};
+static constexpr int LED_PINS[LED_CHANNEL_COUNT] = {19, 18, 17, 16, 4};
+static constexpr int BUTTON_PIN = 0;
